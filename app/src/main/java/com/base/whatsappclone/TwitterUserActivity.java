dc774af15efea991d67ce00c4f72ae5927f46592
dnewintern1 <radhe3127@gmail.com>
2023-10-26 11:18:50 +0530

    to impmelemt list view login done

---
+++ app/src/main/java/com/base/whatsappclone/TwitterUserActivity.java
@@ -0,0 +1,14 @@
+package com.base.whatsappclone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TwitterUserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_twitter_user);
+    }
+}
---
+++ app/src/main/java/com/base/whatsappclone/UserDetails.java
@@ -0,0 +1,20 @@
+package com.base.whatsappclone;
+
+public class UserDetails {
+    private String profileName;
+
+    public UserDetails(){}
+
+
+    public UserDetails(String profileName) {
+        this.profileName = profileName;
+
+    }
+
+
+    public String getProfileName() {
+        return profileName;
+    }
+
+
+}
